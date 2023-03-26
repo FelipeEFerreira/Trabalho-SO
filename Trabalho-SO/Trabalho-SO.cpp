87887f14763fbde7b70ef9c6ca036a5c4a05046a
felipemanuel090283@gmail.com <felipemanuel090283@gmail.com>
2023-03-26 16:35:12 -0400

    Otimização do algoritmo de primalidade

--- Trabalho-SO/Trabalho-SO.cpp
+++ Trabalho-SO/Trabalho-SO.cpp
@@ -417,17 +417,22 @@ void LiberarMatriz(const int linhas) {
 
 /* Contagem dos números primos */
 
+/*
+Código retirado do GeeksforGeeks, disponivel em:
+https://www.geeksforgeeks.org/prime-numbers/
+*/
 bool VerificaPrimalidade(const int n)
 {
     if (n <= 1)
         return false;
     if (n == 2 || n == 3)
         return true;
-    if (!(n & 1)) //verifica se o número é par utilizando operador and bit a bit
+    if (n % 2 == 0 || n % 3 == 0)
         return false;
-    for (int i = 3; i <= n/2; i += 2) //Verifica se é divisivel por algum numero impar
-        if (n % i == 0)
+    for (int i = 5; i <= sqrt(n); i = i + 6)
+        if (n % i == 0 || n % (i + 2) == 0)
             return false;
+
     return true;
 }
 
